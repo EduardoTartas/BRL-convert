d2630368f978e4c67727883a648dfad7550a623b
Eduardo Tartas <eduardos.tartas@gmail.com>
2024-11-25 18:51:44 -0400

    logica finalizada

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,66 @@
 "use strict";
+class Currency {
+    constructor(code, value) {
+        this.code = code;
+        this.value = value;
+    }
+    // Getters
+    getCode() {
+        return this.code;
+    }
+    getValue() {
+        return this.value;
+    }
+    // Setters
+    setCode(code) {
+        this.code = code;
+    }
+    setValue(value) {
+        this.value = value;
+    }
+    convert() {
+        let converted = real.getValue() * this.getValue();
+        return converted;
+    }
+}
+// value update 25/11
+const real = new Currency("BRL", 1);
+const americanDolar = new Currency("US", 5.81);
+const euro = new Currency("EUR", 6.10);
+const libraEsterlina = new Currency("GBP", 7.30);
+const formElement = document.querySelector("form");
+const selectElement = document.querySelector("#currency");
+const amountElement = document.querySelector("#amount");
+const footerElement = document.querySelector("footer");
+const descriptionElement = document.querySelector("#description");
+const resultElement = document.querySelector("#result");
+let convertedValue;
+amountElement.onchange = () => {
+    real.setValue(parseFloat(amountElement.value));
+    console.log(real.getValue());
+};
+formElement.onsubmit = (event) => {
+    event.preventDefault();
+    let selectedCurrency = selectElement.value;
+    footerElement.classList.add("show-result");
+    switch (selectedCurrency) {
+        case "USD":
+            convertedValue = americanDolar.convert();
+            descriptionElement.innerHTML = `${selectedCurrency}$ 1 = R$${americanDolar.getValue()}`;
+            resultElement.innerHTML = `${convertedValue.toFixed(2)} Reais`;
+            break;
+        case "EUR":
+            convertedValue = euro.convert();
+            descriptionElement.innerHTML = `${selectedCurrency}$ 1 = R$${euro.getValue()}`;
+            resultElement.innerHTML = `${convertedValue.toFixed(2)} Reais`;
+            break;
+        case "GBP":
+            convertedValue = libraEsterlina.convert();
+            descriptionElement.innerHTML = `${selectedCurrency}$ 1 = R$${libraEsterlina.getValue()}`;
+            resultElement.innerHTML = `${convertedValue.toFixed(2)} Reais`;
+            break;
+        default:
+            window.alert("error");
+    }
+    console.log(convertedValue);
+};
--- src/index.ts
+++ src/index.ts
@@ -25,16 +25,74 @@ class Currency{
         this.value = value;
     }
 
-    convert(){
-        real.value * this.value
+    convert(): number {
+        let converted:number = real.getValue() * this.getValue();
+        return converted;
     }
 
 }
 
+// value update 25/11
 const real = new Currency("BRL", 1);
 const americanDolar = new Currency("US", 5.81);
 const euro = new Currency("EUR", 6.10);
 const libraEsterlina = new Currency("GBP", 7.30);
+
+
+const formElement = document.querySelector("form") as HTMLFormElement;
+const selectElement = document.querySelector("#currency") as HTMLSelectElement;
+const amountElement = document.querySelector("#amount") as HTMLInputElement;
+const footerElement = document.querySelector("footer") as HTMLElement;
+const descriptionElement = document.querySelector("#description") as HTMLElement
+const resultElement = document.querySelector("#result") as HTMLElement;
+
+let convertedValue:number;
+
+amountElement.onchange = () =>{
+    real.setValue(parseFloat(amountElement.value));
+    console.log(real.getValue());
+}
+
+
+formElement.onsubmit = (event) =>{
+    
+    event.preventDefault();
+    
+    let selectedCurrency = selectElement.value;
+
+    footerElement.classList.add("show-result");
+
+   
+
+    
+
+    switch(selectedCurrency){
+
+        case "USD":
+            convertedValue = americanDolar.convert();
+            descriptionElement.innerHTML = `${selectedCurrency}$ 1 = R$${americanDolar.getValue()}`;
+            resultElement.innerHTML = `${convertedValue.toFixed(2)} Reais`;
+            break;
+
+        case "EUR":
+            convertedValue = euro.convert();
+            descriptionElement.innerHTML = `${selectedCurrency}$ 1 = R$${euro.getValue()}`;
+            resultElement.innerHTML = `${convertedValue.toFixed(2)} Reais`;
+            break;
+
+        case "GBP":
+            convertedValue = libraEsterlina.convert();
+            descriptionElement.innerHTML = `${selectedCurrency}$ 1 = R$${libraEsterlina.getValue()}`; 
+            resultElement.innerHTML = `${convertedValue.toFixed(2)} Reais`;
+            break;
+            
+        default:
+            window.alert("error");    
+    }
+
+    console.log(convertedValue);
+
+}
 
 
 
